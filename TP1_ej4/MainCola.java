80143984551ce94a8d7c3c62582f33a0d860f252
Valentin190306 <101421737+Valentin190306@users.noreply.github.com>
2023-09-02 16:42:54 -0300

    Add files via upload

---
+++ TP1_ej4/MainCola.java
@@ -0,0 +1,28 @@
+import ar.edu.unlu.poo.Cola;
+
+public class MainCola {
+
+    public static void main (String[] args) {
+        int a = 123;
+        byte b = 100;
+        double c = 123456789;
+
+        Cola cola = new Cola();
+
+        System.out.println(" > Cola de clientes: ");
+        for (int i = 1 ; i <= 10 ; i++)
+            cola.encolar("Cliente " + i);
+
+        cola.mostrar();
+        System.out.println(" > El primer cliente de la cola: " + cola.recuperar());
+
+        System.out.println(" > Atendemos a los clientes . . .");
+        for (int i = 0 ; i < 4 ; i++)
+            System.out.println(" > " + cola.desencolar());
+
+        System.out.println(" > La cola esta vacia: " + cola.vacio());
+        System.out.println(" > Clientes que quedan en la cola: " + cola.cantidad());
+
+        cola.mostrar();
+    }
+}
