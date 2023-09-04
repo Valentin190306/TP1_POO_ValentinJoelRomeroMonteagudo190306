7fe0ed14ef5bbd0be576c32b50d655d1bbe0f0c4
Valentin190306 <101421737+Valentin190306@users.noreply.github.com>
2023-09-04 00:36:51 -0300

    Update ListaEnlazada.java

--- ar.edu.unlu.poo/ListaEnlazada.java
+++ ar.edu.unlu.poo/ListaEnlazada.java
@@ -1,5 +1,18 @@
 package ar.edu.unlu.poo;
+/*
+Ejercicio 1: Lista enlazada simple
+Recordando el TAD estudiado en asignaturas previas Lista enlazada simple
 
+Implementar un equivalente orientado a objetos de la Lista enlazada simple. En particular, es necesario que la lista implemente la siguiente interfaz:
+
+- Crear lista.
+- Consultar si la lista está vacía.
+- Consultar la longitud de la lista.
+- Agregar elemento al final de la lista.
+- Eliminar elemento de la lista.
+- Recuperar elemento de la lista.
+- Insertar elemento en la lista en una posición específica.
+*/
 public class ListaEnlazada {
   private Nodo primero;
 
