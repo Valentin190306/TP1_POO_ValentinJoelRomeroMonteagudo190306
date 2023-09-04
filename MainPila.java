aaa4827b43e03785b6405830d17a7caf86ed4734
Valentin190306 <101421737+Valentin190306@users.noreply.github.com>
2023-09-04 00:39:17 -0300

    Update MainPila.java

--- MainPila.java
+++ MainPila.java
@@ -1,5 +1,8 @@
 import ar.edu.unlu.poo.Pila;
-
+/*
+Ejercicio 3: Pila
+Implemente el TAD Pila. Defina cuál debe ser la interfaz a implementar; cuantos objetos están involucrados en la solución; responsabilidades de cada objeto involucrado y ejemplifique el uso.
+*/
 public class MainPila {
   public static void main (String[] args) {
     int a = 123;
