19caece91120612bdc10ac210e5068699466e4da
Valentin190306 <101421737+Valentin190306@users.noreply.github.com>
2023-09-06 21:14:56 -0300

    Update MainEcSegGrad.java

--- MainEcSegGrad.java
+++ MainEcSegGrad.java
@@ -5,8 +5,8 @@ Ejercicio 7
 Se requiere implementar una clase que represente y pueda resolver una ecuación de segundo grado. Dada una ecuación de 2do grado de la forma y = a * x^2 + b * x + c, la fórmula para el cálculo de las raíces es (-b ± √((b^2) - (4 * a * c))) / (2 * a). Una vez que se crea una instancia de la clase no se podrán modificar sus coeficientes. Recordar que puede darse el caso donde exista una única raíz y para que esto ocurra el discriminante ((b^2) - 4 * a * c) debe ser igual a 0. En este último caso, en vez de retornar 2 raíces se debería retornar una sola. Existe el caso también donde necesitemos calcular el valor de y en base al valor de x, que se pasa como parámetro.
 Objetivos:
 
-    _ Definir estructura y comportamiento.
-    _ Crear una clase que utilice a la anterior y que pruebe todos los métodos con varios resultados.
+    1_ Definir estructura y comportamiento.
+    2_ Crear una clase que utilice a la anterior y que pruebe todos los métodos con varios resultados.
 */
 public class MainEcSegGrad {
     public static void main (String[] args) {
